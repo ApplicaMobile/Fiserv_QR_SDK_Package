bfa685468923f1c42ff64aad98715d4fbfaaad7a
Arielo <ascarafia@gmail.com>
2025-04-10 16:30:48 -0300

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -9,15 +9,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SDK_Package",
-            targets: ["SDK_Package"]),
+            targets: ["Fiserv_QR_SDK"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SDK_Package"),
-        .testTarget(
-            name: "SDK_PackageTests",
-            dependencies: ["SDK_Package"]),
+        .binaryTarget(name: "Fiserv_QR_SDK", path: "./Sources/Fiserv_QR_SDK.xcframework"),
+//        .target(
+//            name: "SDK_Package"),
+//        .testTarget(
+//            name: "SDK_PackageTests",
+//            dependencies: ["SDK_Package"]),
     ]
 )
